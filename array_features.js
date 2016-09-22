9625b31fe21cedbb6eb1ad34b3dc76398235c786
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2016-09-21 21:59:57 -0700

    iniital commit

---
+++ array_features.js
@@ -0,0 +1,17 @@
+//create an array of numbers
+var numbers = [1,2,3,4,5]
+
+//create a variable to hold the sum
+
+var sum = 0
+
+//loop over the arrray  incremetning the sum variable
+numbers.forEach( function(number){
+	sum += number;
+});
+
+
+
+//print the sum variable
+
+sum
