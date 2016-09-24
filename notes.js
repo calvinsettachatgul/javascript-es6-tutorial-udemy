b5e1f77d2152ef361e01ac2b617a9dbf35ded179
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2016-09-23 21:00:05 -0700

    notes

--- notes.js
+++ notes.js
@@ -37,4 +37,84 @@ for loop
 */
 
 
+/*
+Map Helper
+==============================
+==============================
+double the value of the array
+
+to mutate or not to mutate?
+whereever possible want to avoid mutating
+
+
+how would this work with map helper?
+*/
+var numbers = [1,2,3];
+
+var doubledNumbers = [];
+
+for(var i = 0; i < numbers.length; i++){
+	doubledNumbers.push(numbers[i] * 2);
+}
+
+console.log(doubledNumbers);
+
+var doubled = numbers.map(function(number){
+	return number * 2;
+});
+
+// each number is being passed into the anon funtion
+
+// whatever is returned?
+
+// take a look at a diagram
+
+
+/*
+using the map function to create a new array
+
+we are producing a new one instead
+
+return keyword is critical
+else you will return undefined
+
+
+place a return keyword inside the mapping function
+
+*/
+
+
+/* more complex map
+some list of data 
+
+
+
+
+*/
+
+
+var cars = [
+	{model: 'Buick', price: 'cheap'},
+	{model: 'Camaro', price: 'expensive'},
+]
+
+
+var prices = cars.map(function(car){
+	return car.price;
+});
+
+
+console.log(prices);
+
+
+// more practical uses of map?
+
+
+// build on day to day basis
+
+// rendering a list of data
+
+// all just rendering list of data
+
+
 
